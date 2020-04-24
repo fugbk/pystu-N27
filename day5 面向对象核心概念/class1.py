7655fd1686ed60d7138d0cba192cbfe1c122a896
liAng <ali6102@163.com>
2020-04-24 10:51:08 +0800

    面向对象核心概念

--- day5 面向对象核心概念/class1.py
+++ day5 面向对象核心概念/class1.py
@@ -1,3 +1,14 @@
 # encoding = utf-8
 __author__ = "Ang Li"
 
+class Person:
+    x = 100
+
+    def eat():
+        print('abc')
+
+
+print(Person)
+print(Person.eat)
+print('----------------')
+print(Person.eat())
